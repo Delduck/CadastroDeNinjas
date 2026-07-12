97b54a0a4614deb683246f7b36ecc6fe0ea0c60d
João Pedro Delduck <jpdelduck@gmail.com>
2026-07-12 18:48:28 -0300

    chore: Inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x dto

--- src/main/java/com/github/Delduck/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/com/github/Delduck/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,38 @@
 package com.github.Delduck.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    // mapear NinjaModel -> DTO
+    public NinjaModel ninjaModeltoDTO(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    // mapear DTO -> NinjaModel
+    public NinjaDTO ninjaDTOtoModel(NinjaModel ninjaModel) {
+
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setRank(ninjaModel.getRank());
+
+        return ninjaDTO;
+    }
 }
+
